e37456c104e3d1f5de8d46b04e7516302af13871
marcus.valencia52 <marcus.valencia52@gmail.com>
2019-07-14 17:39:14 -0700

    Add project files.

---
+++ apitestingprovider/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace apitestingprovider.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ apitestingprovider/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace apitestingprovider.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
