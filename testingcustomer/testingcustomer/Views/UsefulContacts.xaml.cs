12ef5776d2ef9464a74f0aeed8e8cb0c787284b5
marcus.valencia52 <marcus.valencia52@gmail.com>
2020-01-04 22:05:04 -0800

    working on useful contants page

--- testingcustomer/testingcustomer/Views/UsefulContacts.xaml.cs
+++ testingcustomer/testingcustomer/Views/UsefulContacts.xaml.cs
@@ -28,5 +28,78 @@ namespace testingcustomer.Views
                 DisplayAlert("Unable to make calls at this time", "Please try again later", "OK");
             }
         }
+
+        private async void coywebsitebutton(object sender, EventArgs e)
+        {
+            try
+            {
+                await Browser.OpenAsync("https://www.coylandtechs.com/", BrowserLaunchMode.SystemPreferred);
+
+            }
+            catch (Exception ex) 
+            {
+                await DisplayAlert("Unable to open a browser at this time", "Please try again later", "OK");
+            }
+        }
+
+        private void digphonebutton(object sender, EventArgs e)
+        {
+            try
+            {
+                PhoneDialer.Open("811");
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Unable to make calls at this time", "Please try again later", "OK");
+            }
+        }
+
+        private async void digwebsitebutton(object sender, EventArgs e)
+        {
+            try
+            {
+                await Browser.OpenAsync("https://www.digalert.org/contact", BrowserLaunchMode.SystemPreferred);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Unable to open a browser at this time", "Please try again later", "OK");
+            }
+        }
+
+        private void rachiophonebutton(object sender, EventArgs e)
+        {
+            try
+            {
+                PhoneDialer.Open("8444722446");
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Unable to make calls at this time", "Please try again later", "OK");
+            }
+        }
+        private async void rachiowebsitebutton(object sender, EventArgs e)
+        {
+            try
+            {
+                await Browser.OpenAsync("https://rachio.com/", BrowserLaunchMode.SystemPreferred);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Unable to open a browser at this time", "Please try again later", "OK");
+            }
+        }
+
+        private async void pinwebsitebutton(object sender, EventArgs e)
+        {
+            try
+            {
+                await Browser.OpenAsync("https://www.pinterest.com/", BrowserLaunchMode.SystemPreferred);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Unable to open a browser at this time", "Please try again later", "OK");
+            }
+        }
+
     }
 }
