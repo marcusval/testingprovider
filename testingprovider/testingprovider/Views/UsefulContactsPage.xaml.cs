2257c83bf2e992f87d8990ca7df0506361c2c09d
marcus.valencia52 <marcus.valencia52@gmail.com>
2020-01-09 19:48:21 -0800

    Working & Complete useful contacts page. Uses a CarouselPage in a tabbed page view. Example of how to use phone launcher and web browser launcher xamarin essientals api.

--- testingprovider/testingprovider/Views/UsefulContactsPage.xaml.cs
+++ testingprovider/testingprovider/Views/UsefulContactsPage.xaml.cs
@@ -3,18 +3,103 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+using Xamarin.Essentials;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace testingprovider.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class UsefulContactsPage : ContentPage
+    public partial class UsefulContactsPage : CarouselPage
     {
         public UsefulContactsPage()
         {
             InitializeComponent();
         }
+
+        private void coyphonebutton(object sender, EventArgs e)
+        {
+            try
+            {
+                PhoneDialer.Open("9513940848");
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Unable to make calls at this time", "Please try again later", "OK");
+            }
+        }
+
+        private async void coywebsitebutton(object sender, EventArgs e)
+        {
+            try
+            {
+                await Browser.OpenAsync("https://www.coylandtechs.com/", BrowserLaunchMode.SystemPreferred);
+
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Unable to open a browser at this time", "Please try again later", "OK");
+            }
+        }
+
+        private void digphonebutton(object sender, EventArgs e)
+        {
+            try
+            {
+                PhoneDialer.Open("811");
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Unable to make calls at this time", "Please try again later", "OK");
+            }
+        }
+
+        private async void digwebsitebutton(object sender, EventArgs e)
+        {
+            try
+            {
+                await Browser.OpenAsync("https://www.digalert.org/contact", BrowserLaunchMode.SystemPreferred);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Unable to open a browser at this time", "Please try again later", "OK");
+            }
+        }
+
+        private void rachiophonebutton(object sender, EventArgs e)
+        {
+            try
+            {
+                PhoneDialer.Open("8444722446");
+            }
+            catch (Exception ex)
+            {
+                DisplayAlert("Unable to make calls at this time", "Please try again later", "OK");
+            }
+        }
+        private async void rachiowebsitebutton(object sender, EventArgs e)
+        {
+            try
+            {
+                await Browser.OpenAsync("https://rachio.com/", BrowserLaunchMode.SystemPreferred);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Unable to open a browser at this time", "Please try again later", "OK");
+            }
+        }
+
+        private async void pinwebsitebutton(object sender, EventArgs e)
+        {
+            try
+            {
+                await Browser.OpenAsync("https://www.pinterest.com/", BrowserLaunchMode.SystemPreferred);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Unable to open a browser at this time", "Please try again later", "OK");
+            }
+        }
+
     }
 }
